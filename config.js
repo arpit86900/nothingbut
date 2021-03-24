e03240f510ef839b9c77cd724d3685770c5ba0c2
arpit86900 <77879722+arpit86900@users.noreply.github.com>
2021-03-24 17:46:16 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+    token: 'ODIzNDYyNjgwMTA2MjM3OTgz.YFhLYg.NRwDi3lY_F9OazFeE1pInc7L6-g',
+    prefix: '<'
+}
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+const Discord = require("discord.js");
+const fs = require("fs");
+
+
+const client = new Discord.Client();
+const config = require("./config.js");
+client.config = config;
+client.queue = new Map()
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
+
+client.commands = new Discord.Collection()
+
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`Attempting to load command ${commandName}`);
+    client.commands.set(commandName, props);
+  });
+});
+
+client.login(config.token);
